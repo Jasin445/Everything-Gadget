d3767fc13f21d334c8f8e70ccaadb59a85142cfa
Jasin445 <daganajason72@gmail.com>
2025-02-04 06:16:25 +0100

    two of them

--- index.js
+++ index.js
@@ -69,4 +69,111 @@ function getUniqueItems1(arr) {
 
 }
 
+function getUniqueItems2(arr) {
+  return new Set([arr])
+}
+
+getUniqueItems2(array)
+
 console.log(getUniqueItems1(array))
+
+
+const inputs = document.getElementById("weather");
+const submitted = document.getElementById("start");
+const wrap = document.createElement('div');
+const img = document.querySelector('.imgs');
+img.classList.add('hide')
+  wrap.id = 'memo';
+document.querySelectorAll('.container')[1].append(wrap);
+let p = document.createElement("p")
+document.querySelectorAll('.container')[1].append(p)
+
+
+
+async function getWeatherData(arr) {
+
+  try {
+   
+    let url = `https://api.weatherapi.com/v1/current.json?key=cc188cc6067b493aae3194433250302&q=${arr}`
+    
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Server down!")
+    }
+    
+    let data = await response.json();
+    console.log(data)
+    return data;
+  } catch (err) {
+   
+    img.classList.add('hide')
+    return null; 
+  }
+}
+
+async function inputData() {
+  img.classList.remove('hide')
+  const myValue = inputs.value.trim();
+  console.log(myValue)
+  
+  displayerror(" ")
+  wrap.innerHTML = "";
+
+  if (!myValue) {
+    displayerror("Please enter a location!");
+    img.classList.add('hide');
+    return;
+  }
+  
+
+    let data = await getWeatherData(myValue);
+
+    if (!data || !data.location) {
+      displayerror("Invalid Location or Please Check your Network connection and try again!");
+      return;
+    }
+    console.log(data.location.name.toLowerCase(),data.location.region.toLowerCase(), data.location.country.toLowerCase(), myValue)
+    if (data.location.name.toLowerCase() === myValue.toLowerCase() || data.location.region.toLowerCase() === myValue.toLowerCase() || data.location.country.toLowerCase() === myValue.toLowerCase()) {
+    
+
+    
+      console.log(data.current.condition.icon)
+      img.classList.add('hide')
+      let div = ` <div class="status">
+        <p>Country: <span class="country">${data.location.country}</span></p>
+        <p>Place: <span class="country">${data.location.name}</span></p>
+        <p>Region: <span class="country">${data.location.region}</span></p>
+        <p>Temperature: <span class="country">${data.current.temp_c}&degC</span></p>
+        <p>Humidity: <span class="country">${data.current.humidity}%</span></p>
+        <div class="condi">
+          <p>Condition: ${data.current.condition.text} </p>
+          <img class="shine" src="${data.current.condition.icon}" alt="">
+        </div>
+      </div>`;
+    
+      wrap.innerHTML = div;
+      
+    } else {
+      img.classList.add('hide')
+
+      displayerror("Location Mismatch!")
+      
+      return;
+    }
+  
+
+    
+ 
+
+}
+
+function displayerror(msg) {
+ 
+  p.classList.add('error')
+  return p.innerHTML = msg
+  
+}
+
+
+
+submitted.addEventListener('click', inputData)
